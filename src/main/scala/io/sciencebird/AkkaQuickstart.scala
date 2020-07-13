0e86806e2bc0ff4be7969432126ef5a1448135d0
Jeronimo Carlos <jeronimo.carlos@ing.austral.edu.ar>
2020-07-13 14:00:12 -0300

    Produce kafka messages from an actor

--- src/main/scala/io/sciencebird/AkkaQuickstart.scala
+++ src/main/scala/io/sciencebird/AkkaQuickstart.scala
@@ -2,111 +2,36 @@
 package io.sciencebird
 
 import akka.Done
-import akka.actor.typed.ActorRef
-import akka.actor.typed.ActorSystem
-import akka.actor.typed.Behavior
-import akka.actor.typed.delivery.ConsumerController.RegisterToProducerController
+import akka.actor.typed.{ActorSystem, Behavior}
 import akka.actor.typed.scaladsl.Behaviors
-import akka.kafka.scaladsl.Consumer._
-import akka.kafka.scaladsl.{ Committer, Consumer, Producer }
-import akka.kafka.{ CommitterSettings, ConsumerSettings, ProducerSettings, Subscriptions }
-import akka.stream.scaladsl.{ Flow, Keep, Sink, Source }
-import io.sciencebird.GreeterMain.SayHello
+import akka.actor.typed.scaladsl.adapter._
+import akka.actor.{ActorSystem => ClassicActorSystem}
+import akka.kafka.scaladsl.{Consumer, Producer}
+import akka.kafka.{CommitterSettings, ConsumerSettings, ProducerSettings, Subscriptions}
+import akka.stream.Materializer
+import akka.stream.scaladsl.{Keep, Sink, Source}
+import io.sciencebird.AkkaQuickstart.{producerSettings, topic}
 import org.apache.kafka.clients.consumer.ConsumerConfig
-import org.apache.kafka.clients.producer.{ ProducerConfig, ProducerRecord }
-import org.apache.kafka.common.TopicPartition
-import org.apache.kafka.common.serialization.{ ByteArrayDeserializer, StringDeserializer, StringSerializer }
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.{StringDeserializer, StringSerializer}
 
-import scala.concurrent.{ ExecutionContext, Future }
-
-//#greeter-actor
-object Greeter {
-  final case class Greet(whom: String, replyTo: ActorRef[Greeted])
-  final case class Greeted(whom: String, from: ActorRef[Greet])
-
-  def apply(): Behavior[Greet] = Behaviors.receive { (context, message) =>
-    context.log.info("Hello {}!", message.whom)
-    //#greeter-send-messages
-    message.replyTo ! Greeted(message.whom, context.self)
-    //#greeter-send-messages
-    Behaviors.same
-  }
-}
-//#greeter-actor
-
-//#greeter-bot
-object GreeterBot {
-
-  def apply(max: Int): Behavior[Greeter.Greeted] =
-    bot(0, max)
-
-  private def bot(greetingCounter: Int, max: Int): Behavior[Greeter.Greeted] =
-    Behaviors.receive { (context, message) =>
-      val n = greetingCounter + 1
-      context.log.info("Greeting {} for {}", n, message.whom)
-      if (n == max)
-        Behaviors.stopped
-      else {
-        message.from ! Greeter.Greet(message.whom, context.self)
-        bot(n, max)
-      }
-    }
-}
-//#greeter-bot
-
-//#greeter-main
-object GreeterMain {
-
-  final case class SayHello(name: String)
-
-  def apply(): Behavior[SayHello] =
-    Behaviors.setup { context =>
-      //#create-actors
-      val greeter = context.spawn(Greeter(), "greeter")
-      //#create-actors
-
-      Behaviors.receiveMessage { message =>
-        //#create-actors
-        val replyTo = context.spawn(GreeterBot(max = 3), message.name)
-        //#create-actors
-        greeter ! Greeter.Greet(message.name, replyTo)
-        Behaviors.same
-      }
-    }
-}
-//#greeter-main
+import scala.concurrent.{ExecutionContext, Future}
+import scala.language.postfixOps
 
 //#main-class
 object AkkaQuickstart extends App {
-  //#actor-system
-  val greeterMain: ActorSystem[GreeterMain.SayHello] = ActorSystem(GreeterMain(), "AkkaQuickStart")
-  //#actor-system
+  val system: ClassicActorSystem = ClassicActorSystem("producer-test")
+  implicit val typedSystem: ActorSystem[Nothing] = system.toTyped
 
-  //#main-send-messages
-  greeterMain ! SayHello("Charles")
-  //#main-send-messages
-
-  implicit val system = greeterMain.classicSystem
   val config          = system.settings.config.getConfig("our-kafka-consumer")
   val producerConfig  = system.settings.config.getConfig("our-kafka-producer")
   val consumerSettings = ConsumerSettings(config, new StringDeserializer, new StringDeserializer)
     .withGroupId("g1")
-    .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+    .withProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true") // avoid re-reading all messages in the queue
   val topic                         = "test"
   implicit val ec: ExecutionContext = system.dispatcher
 
   val committerSettings = CommitterSettings(system)
-
-//  val control: DrainingControl[Done] =
-//    Consumer
-//      .committableSource(consumerSettings, Subscriptions.topics(topic))
-//      .mapAsync(1) { msg =>
-//        business(msg.record.key, msg.record.value)
-//          .map(_ => msg.committableOffset)
-//      }
-//      .toMat(Committer.sink(committerSettings))(DrainingControl.apply)
-//      .run()
-
   val (consumerControl, streamComplete) =
     Consumer
       .plainSource(
@@ -120,26 +45,62 @@ object AkkaQuickstart extends App {
       .run()
 
   def business(key: String, value: String): Future[Done] = {
-    println(s"Received $key : $value")
+    typedSystem.log.info(s"Received $key : $value")
     Future.successful(Done)
   }
 
   val producerSettings =
     ProducerSettings(producerConfig, new StringSerializer, new StringSerializer)
 
-  (1 to 3).foreach { n =>
-    println(s"Starting round: $n")
-    Source(n * 1000 to n * 1100)
-      .map(_.toString)
-      .map(value => new ProducerRecord[String, String](topic, value))
-      .runWith(Producer.plainSink(producerSettings))
-    println(s"Finishing round $n")
-    Thread.sleep(1000)
+  val limit = 15 // number of rounds
+  val delay = 2 // interval between rounds
+  system.spawn(
+    ProducerActor(limit, delay),
+    "producer-system"
+  )
+
+  Thread.sleep(delay * (limit + 1) * 1000)
+  consumerControl.shutdown()
+  sys.exit(0)
+}
+
+object ProducerActor {
+  import scala.concurrent.duration._
+
+  sealed trait ProducerMessage
+  case object Tick extends ProducerMessage
+
+  def apply(
+    limit: Int,
+    delay: Int
+  )(implicit ec: ExecutionContext, mat: Materializer): Behavior[ProducerMessage] = {
+    Behaviors.setup { context =>
+      Behaviors.withTimers { scheduler =>
+        context.log.debug("starting timer")
+        var counter = 0
+        scheduler.startTimerWithFixedDelay(counter, Tick, delay seconds)
+
+        def terminate: Behavior[ProducerMessage] = {
+          scheduler.cancelAll()
+          Behaviors.stopped
+        }
+
+        Behaviors.receiveMessage {
+          case Tick =>
+            if(counter == limit) terminate
+            else {
+              context.log.debug(s"Starting round: $counter")
+              Source(0 to counter * 1000)
+                .map(_.toString)
+                .map(value => new ProducerRecord[String, String](topic, counter.toString, value))
+                .runWith(Producer.plainSink(producerSettings))
+                .map(_ => counter += 1)
+
+              Behaviors.same
+            }
+        }
+      }
+    }
   }
 
-  Thread.sleep(200 * 1000)
-  consumerControl.shutdown()
-
 }
-//#main-class
-//#full-example
--- src/test/scala/io/sciencebird/AkkaQuickstartSpec.scala
+++ src/test/scala/io/sciencebird/AkkaQuickstartSpec.scala
@@ -2,24 +2,12 @@
 package io.sciencebird
 
 import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
-import io.sciencebird.Greeter.Greet
-import io.sciencebird.Greeter.Greeted
 import org.scalatest.wordspec.AnyWordSpecLike
 
 //#definition
 class AkkaQuickstartSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
 //#definition
 
-  "A Greeter" must {
-    //#test
-    "reply to greeted" in {
-      val replyProbe = createTestProbe[Greeted]()
-      val underTest = spawn(Greeter())
-      underTest ! Greet("Santa", replyProbe.ref)
-      replyProbe.expectMessage(Greeted("Santa", underTest.ref))
-    }
-    //#test
-  }
 
 }
 //#full-example
