0ad9b9e7ea66cdb8619965ecf4755d1f7ab004da
Pablo Cavallo <pac@codebox.com.ar>
2020-08-27 20:04:18 +0000

    Update scalatest to 3.2.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,6 +14,6 @@ object Dependencies {
   lazy val cats                   = "org.typelevel"     %% "cats-core"                % "2.1.1"
   lazy val logbackClassic         = "ch.qos.logback"    % "logback-classic"           % "1.2.3"
   lazy val scalaCheck             = "org.scalacheck"    %% "scalacheck"               % "1.14.3"
-  lazy val scalaTest              = "org.scalatest"     %% "scalatest"                % "3.2.1"
+  lazy val scalaTest              = "org.scalatest"     %% "scalatest"                % "3.2.2"
   lazy val scalaTestPlus          = "org.scalatestplus" %% "scalacheck-1-14"          % "3.2.0.0"
 }
