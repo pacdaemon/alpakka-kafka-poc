6729ea28dc68e921c79f8acbfd1400eacd42f9b5
Pablo Cavallo <pac@codebox.com.ar>
2021-02-18 20:05:10 +0000

    Update scalacheck to 1.15.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
   lazy val akkaStreamKafka        = "com.typesafe.akka" %% "akka-stream-kafka"        % "2.0.7"
   lazy val cats                   = "org.typelevel"     %% "cats-core"                % "2.1.1"
   lazy val logbackClassic         = "ch.qos.logback"    % "logback-classic"           % "1.2.3"
-  lazy val scalaCheck             = "org.scalacheck"    %% "scalacheck"               % "1.15.2"
+  lazy val scalaCheck             = "org.scalacheck"    %% "scalacheck"               % "1.15.3"
   lazy val scalaTest              = "org.scalatest"     %% "scalatest"                % "3.2.3"
   lazy val scalaTestPlus          = "org.scalatestplus" %% "scalacheck-1-14"          % "3.2.0.0"
 }
